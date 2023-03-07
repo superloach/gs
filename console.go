1e5530365255d017b4d3cf173e36b750b1372d24
Terra Brown <superloach42@gmail.com>
2023-03-07 16:38:27 -0500

    fix dirxml comment

--- console.go
+++ console.go
@@ -97,7 +97,7 @@ func (c ConsoleType) Dir(o Object) {
 	_, _ = c.Call("dir", o)
 }
 
-// The console.dirxml() method displays an interactive tree of the descendant elements of the specified XML/HTML element. If it is not possible to display as an element the JavaScript Object view is shown instead. The output is presented as a hierarchical listing of expandable nodes that let you see the contents of child nodes.
+// DirXML displays an interactive tree of the descendant elements of the specified XML/HTML element. If it is not possible to display as an element the JavaScript Object view is shown instead. The output is presented as a hierarchical listing of expandable nodes that let you see the contents of child nodes.
 //
 //	c.DirXML(object)
 func (c ConsoleType) DirXML(o Object) {
